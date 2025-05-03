89a1fc92efe39b7d940e7ca2aea6629e9b4d8e43
@irunga1 <irunga1@yahoo.com>
2025-05-03 07:49:56 -0600

    remove comments at queryselector

--- libs/QuerySelector.js
+++ libs/QuerySelector.js
@@ -1,8 +1,8 @@
 export const $ = (el) => {
     if (el.startsWith('#')) {
-        return document.querySelector(el); // Retorna un único elemento para ID
+        return document.querySelector(el); 
     } else if (el.startsWith('.')) {
-        return document.querySelectorAll(el); // Retorna una lista de nodos para clases
+        return document.querySelectorAll(el); 
     }
-    return null; // Si no es ni clase ni ID, retorna nul
+    return null; 
 }
